270cb68a52f8859407c5adf5f8d2ad283058669b
PretomGhosh <pretomghoshcuet@gmail.com>
2023-09-01 14:58:13 -0400

    After required environment and packages added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Cell_segmentation_problem_with_Azure_Deployment',
+    version = '0.0.0',
+    author = 'Pretom Ghosh',
+    author_email= 'pretomghoshcuet@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
